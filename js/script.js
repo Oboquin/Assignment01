cf407517972e56c4fb1c9bdd5e861b5f694fe0e9
orboquin <orboquin@gmail.com>
2026-06-17 13:54:51 -0700

    Updated to Step 7 in JS and Added H1 Title on index.html

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// STEP 1
+// let some_Month
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryfunction
+
+// STEP 2 
+//Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
+
+//numeric literal expression
+// 7.9
+
+//string literal expression
+// "my name is orlando"
+
+//Boolean literal expression
+// true
+
+// STEP 3
+// Give me two examples of complex / variable expressions.
+// let sum = 32 + 4
+// (4 * 8) 
+// Student + name
+
+// STEP 4
+//Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
+// let firstName;
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let zipCode;
+// let yourAge;
+// let referralSource;
+// may mayWeContactYou; 
+
+// STEP 5
+// let firstName = "Orlando";
+// let lastName = "Boquin";
+// let city = "San Diego";
+
+//STEP 6
+// Create a variable. Add a number and a string and display the coerced result in the browser’s console window.
+//let studentName = 'Orlando'
+//alert('This is ' + studentName  + ' variable exercise number ' + 1 + '. ' + ' Now, ' + studentName + ' has to ignore the worldcup and practice javascript everyday. ')
+
+//STEP 7 
+//Create two variables.
+//For the first variable, add a Boolean and a string and display the coerced result.
+//For the second variable, add a number and a Boolean and display the coerced result.
+
+//let firstName = 'Orlando'
+//let isAStudent = true
+//console.log( firstName + ' is a ' + isAStudent + ' student')
+
+//let yearsOfExperience = 2
+//let hasExperience = false
+//console.log ('Orlando has ' + yearsOfExperience + ' years of javascript experience: ' + hasExperience)
